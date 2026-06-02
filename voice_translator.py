639c9f31cf519d9c183d373b8c3615c2df8b99f3
uditpal518-coder <uditpal518@gmail.com>
2026-06-02 15:19:26 +0530

    Update voice_translator.py

--- voice_translator.py
+++ voice_translator.py
@@ -4,8 +4,9 @@ from deep_translator import GoogleTranslator
 import gtts
 import io
 import base64
+from audio_recorder_streamlit import audio_recorder
 
-st.set_page_config(page_title='AI_Translator',page_icon='',layout='wide')
+st.set_page_config(page_title='AI_Translator',page_icon='🎤',layout='wide')
 st.title("Voice Translator")
 
 if "input_text" not in st.session_state:
@@ -16,18 +17,25 @@ col1,col2 = st.columns(2)
 with col1:
     st.subheader("English")
     if st.button("start"):
+        audio_bytes = audio_recorder(
+        text="Click to record", 
+        recording_color="#e81416", 
+        neutral_color="#6aa36f"
+    )
+    if audio_bytes:
         r=sr.Recognizer()
-        with sr.Microphone() as mic:
-            st.text('say something...')
-            r.adjust_for_ambient_noise(mic, duration=1)
-            try:
-                audio=r.listen(mic)
+        audio_file = io.BytesIO(audio_bytes)
+        st.text('say something...')
+        r.adjust_for_ambient_noise(mic, duration=1)
+        try:
+            with sr.AudioFile(audio_file) as source:
+                audio_data = r.record(source)
                 st.session_state.input_text = r.recognize_google(audio)
-        
-                # translator=GoogleTranslator(source='auto',target='hi')
-                # st.session_state.translated_text = translator.translate(st.session_state.input_text)
-            except Exception as e:
-                st.error("your voice not clear Try again!")
+    
+            # translator=GoogleTranslator(source='auto',target='hi')
+            # st.session_state.translated_text = translator.translate(st.session_state.input_text)
+        except Exception as e:
+            st.error("your voice not clear Try again!")
     typed_text=st.text_area("Input Text", value=st.session_state.input_text, height=150)
     if typed_text != st.session_state.input_text:
         st.session_state.input_text = typed_text
